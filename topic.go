5a410029242b07db2e3bdd8977cb9123b073bb24
Matthew Lugo <matthewlugo746@gmail.com>
2025-06-23 12:31:59 -0400

    Added: topic

---
+++ topic.go
@@ -0,0 +1,14 @@
+package polymarketdataclient
+
+type Topic string
+
+const (
+	// TopicActivity is the topic for activity
+	TopicActivity Topic = "activity"
+
+	// TopicComments is the topic for comments
+	TopicComments Topic = "comments"
+
+	// TopicRfq is the topic for RFQ
+	TopicRfq Topic = "rfq"
+)
